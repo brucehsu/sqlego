6e8e85dc851d3fda2eb2d421facc263c1e31d202
Szu-Kai Hsu (brucehsu) <brucehsu@gmail.com>
2015-01-06 10:50:23 +0800

    use gofmt to reformat code

--- ast.go
+++ ast.go
@@ -1,44 +1,44 @@
 package sqlego
 
 const (
-  STMT_INSERT uint = iota
-  STMT_SELECT
-  STMT_UPDATE
-  STMT_DELETE
-  CLAU_WHERE
-  CLAU_FROM
-  LIST_COLS
-  LIST_CONDS
-  NODE_PRED
-  NODE_AND
-  NODE_OR
-  PRED_EQ
-  PRED_NEQ
-  PRED_GT
-  PRED_GTE
-  PRED_LT
-  PRED_LTE
+	STMT_INSERT uint = iota
+	STMT_SELECT
+	STMT_UPDATE
+	STMT_DELETE
+	CLAU_WHERE
+	CLAU_FROM
+	LIST_COLS
+	LIST_CONDS
+	NODE_PRED
+	NODE_AND
+	NODE_OR
+	PRED_EQ
+	PRED_NEQ
+	PRED_GT
+	PRED_GTE
+	PRED_LT
+	PRED_LTE
 )
 
 type AST struct {
-  Type uint
-  Values []string
-  children []interface{}
+	Type     uint
+	Values   []string
+	children []interface{}
 }
 
 type Statement struct {
-  AST
-  table string
-  columns []string
+	AST
+	table   string
+	columns []string
 }
 
 type Clause struct {
-  AST
+	AST
 }
 
 type Predicate struct {
-  AST
-  operator uint
-  first string
-  second string
+	AST
+	operator uint
+	first    string
+	second   string
 }
--- sqlego.go
+++ sqlego.go
@@ -1,155 +1,155 @@
 package sqlego
 
 import (
-  "fmt"
-  "bytes"
-  "strings"
-  "reflect"
+	"bytes"
+	"fmt"
+	"reflect"
+	"strings"
 )
 
-func Select(table string, columns []string) (*Statement) {
-  node := &Statement{table: table, columns: columns}
-  node.Type = STMT_SELECT
-  return node
+func Select(table string, columns []string) *Statement {
+	node := &Statement{table: table, columns: columns}
+	node.Type = STMT_SELECT
+	return node
 }
 
-func Insert(table string, values map[string]string) (*Statement) {
-  node := &Statement{table: table}
-  for k, v := range values {
-    node.columns = append(node.columns, k)
-    node.Values = append(node.Values, v)
-  }
-  node.Type = STMT_INSERT
-  return node
+func Insert(table string, values map[string]string) *Statement {
+	node := &Statement{table: table}
+	for k, v := range values {
+		node.columns = append(node.columns, k)
+		node.Values = append(node.Values, v)
+	}
+	node.Type = STMT_INSERT
+	return node
 }
 
-func Update(table string, values map[string]string) (*Statement) {
-  node := &Statement{table: table}
-  for k, v := range values {
-    node.columns = append(node.columns, k)
-    node.Values = append(node.Values, v)
-  }
-  node.Type = STMT_UPDATE
-  return node
+func Update(table string, values map[string]string) *Statement {
+	node := &Statement{table: table}
+	for k, v := range values {
+		node.columns = append(node.columns, k)
+		node.Values = append(node.Values, v)
+	}
+	node.Type = STMT_UPDATE
+	return node
 }
 
-func Delete(table string) (*Statement) {
-  node := &Statement{table: table}
-  node.Type = STMT_DELETE
-  return node
+func Delete(table string) *Statement {
+	node := &Statement{table: table}
+	node.Type = STMT_DELETE
+	return node
 }
 
-func newTernaryPredicate(operand string, operand_second string, operator uint) (*Predicate) {
-  pred := &Predicate{first: operand, second: operand_second, operator: operator}
-  pred.Type = NODE_PRED
-  return pred
+func newTernaryPredicate(operand string, operand_second string, operator uint) *Predicate {
+	pred := &Predicate{first: operand, second: operand_second, operator: operator}
+	pred.Type = NODE_PRED
+	return pred
 }
 
-func Eq(operand string, operand_second string) (*Predicate) {
-  return newTernaryPredicate(operand, operand_second, PRED_EQ)
+func Eq(operand string, operand_second string) *Predicate {
+	return newTernaryPredicate(operand, operand_second, PRED_EQ)
 }
 
-func Neq(operand string, operand_second string) (*Predicate) {
-  return newTernaryPredicate(operand, operand_second, PRED_NEQ)
+func Neq(operand string, operand_second string) *Predicate {
+	return newTernaryPredicate(operand, operand_second, PRED_NEQ)
 }
 
-func Gt(operand string, operand_second string) (*Predicate) {
-  return newTernaryPredicate(operand, operand_second, PRED_GT)
+func Gt(operand string, operand_second string) *Predicate {
+	return newTernaryPredicate(operand, operand_second, PRED_GT)
 }
 
-func Gte(operand string, operand_second string) (*Predicate) {
-  return newTernaryPredicate(operand, operand_second, PRED_GTE)
+func Gte(operand string, operand_second string) *Predicate {
+	return newTernaryPredicate(operand, operand_second, PRED_GTE)
 }
 
-func Lt(operand string, operand_second string) (*Predicate) {
-  return newTernaryPredicate(operand, operand_second, PRED_LT)
+func Lt(operand string, operand_second string) *Predicate {
+	return newTernaryPredicate(operand, operand_second, PRED_LT)
 }
 
-func Lte(operand string, operand_second string) (*Predicate) {
-  return newTernaryPredicate(operand, operand_second, PRED_LTE)
+func Lte(operand string, operand_second string) *Predicate {
+	return newTernaryPredicate(operand, operand_second, PRED_LTE)
 }
 
-func (node *Statement) Where(pred *Predicate) (*Statement) {
-  where_clause := &Clause{}
-  where_clause.Type = CLAU_WHERE
-  node.children = append(node.children, where_clause)
-  where_clause.children = append(where_clause.children, pred)
-  return node
+func (node *Statement) Where(pred *Predicate) *Statement {
+	where_clause := &Clause{}
+	where_clause.Type = CLAU_WHERE
+	node.children = append(node.children, where_clause)
+	where_clause.children = append(where_clause.children, pred)
+	return node
 }
 
 func (node *Statement) Compile() string {
-  var buffer bytes.Buffer
-  switch node.Type {
-  case STMT_SELECT:
-    buffer.WriteString("SELECT ")
-    buffer.WriteString(strings.Join(node.columns, ","))
-    buffer.WriteString(" FROM ")
-    buffer.WriteString(node.table)
-  case STMT_INSERT:
-    buffer.WriteString("INSERT INTO ")
-    buffer.WriteString(node.table)
-    buffer.WriteString(" (")
-    buffer.WriteString(strings.Join(node.columns,","))
-    buffer.WriteString(") VALUES (")
-    buffer.WriteString(strings.Join(node.Values, ","))
-    buffer.WriteString(")")
-  case STMT_UPDATE:
-    buffer.WriteString("UPDATE ")
-    buffer.WriteString(node.table)
-    buffer.WriteString(" SET ")
-    predicates := []string{}
-    for idx, col := range(node.columns) {
-      predicates = append(predicates, fmt.Sprintf("%s=%s", col, node.Values[idx]))
-    }
-    buffer.WriteString(strings.Join(predicates, ","))
-  case STMT_DELETE:
-    buffer.WriteString("DELETE FROM ")
-    buffer.WriteString(node.table)
-  }
+	var buffer bytes.Buffer
+	switch node.Type {
+	case STMT_SELECT:
+		buffer.WriteString("SELECT ")
+		buffer.WriteString(strings.Join(node.columns, ","))
+		buffer.WriteString(" FROM ")
+		buffer.WriteString(node.table)
+	case STMT_INSERT:
+		buffer.WriteString("INSERT INTO ")
+		buffer.WriteString(node.table)
+		buffer.WriteString(" (")
+		buffer.WriteString(strings.Join(node.columns, ","))
+		buffer.WriteString(") VALUES (")
+		buffer.WriteString(strings.Join(node.Values, ","))
+		buffer.WriteString(")")
+	case STMT_UPDATE:
+		buffer.WriteString("UPDATE ")
+		buffer.WriteString(node.table)
+		buffer.WriteString(" SET ")
+		predicates := []string{}
+		for idx, col := range node.columns {
+			predicates = append(predicates, fmt.Sprintf("%s=%s", col, node.Values[idx]))
+		}
+		buffer.WriteString(strings.Join(predicates, ","))
+	case STMT_DELETE:
+		buffer.WriteString("DELETE FROM ")
+		buffer.WriteString(node.table)
+	}
 
-  // Generate WHERE clause if exists
-  buffer.WriteString(compileWhere(node.children))
+	// Generate WHERE clause if exists
+	buffer.WriteString(compileWhere(node.children))
 
-  buffer.WriteString(";")
-  return buffer.String()
+	buffer.WriteString(";")
+	return buffer.String()
 }
 
-func compileWhere(children []interface{}) (string) {
-  var buffer bytes.Buffer
-  for _, obj := range children {
-    if reflect.TypeOf(obj) == reflect.TypeOf(&Clause{}) {
-      clause := obj.(*Clause)
-      if clause.Type == CLAU_WHERE {
-        buffer.WriteString(" WHERE ")
-        // We only have 1 predicate for now
-        buffer.WriteString(compilePredicates(clause.children))
-      }
-    }
-  }
-  return buffer.String()
+func compileWhere(children []interface{}) string {
+	var buffer bytes.Buffer
+	for _, obj := range children {
+		if reflect.TypeOf(obj) == reflect.TypeOf(&Clause{}) {
+			clause := obj.(*Clause)
+			if clause.Type == CLAU_WHERE {
+				buffer.WriteString(" WHERE ")
+				// We only have 1 predicate for now
+				buffer.WriteString(compilePredicates(clause.children))
+			}
+		}
+	}
+	return buffer.String()
 }
 
-func compilePredicates(children []interface{}) (string) {
-  // We assume given argument is a list of predicate nodes
-  var buffer bytes.Buffer
-  for _, node := range children {
-    pred := node.(*Predicate)
-    buffer.WriteString(pred.first)
-    switch pred.operator {
-    case PRED_EQ:
-      buffer.WriteString("=")
-    case PRED_NEQ:
-      buffer.WriteString("<>")
-    case PRED_GT:
-      buffer.WriteString(">")
-    case PRED_GTE:
-      buffer.WriteString(">=")
-    case PRED_LT:
-      buffer.WriteString("<")
-    case PRED_LTE:
-      buffer.WriteString("<=")
-    }
-    buffer.WriteString(pred.second)
-  }
-  return buffer.String()
+func compilePredicates(children []interface{}) string {
+	// We assume given argument is a list of predicate nodes
+	var buffer bytes.Buffer
+	for _, node := range children {
+		pred := node.(*Predicate)
+		buffer.WriteString(pred.first)
+		switch pred.operator {
+		case PRED_EQ:
+			buffer.WriteString("=")
+		case PRED_NEQ:
+			buffer.WriteString("<>")
+		case PRED_GT:
+			buffer.WriteString(">")
+		case PRED_GTE:
+			buffer.WriteString(">=")
+		case PRED_LT:
+			buffer.WriteString("<")
+		case PRED_LTE:
+			buffer.WriteString("<=")
+		}
+		buffer.WriteString(pred.second)
+	}
+	return buffer.String()
 }
