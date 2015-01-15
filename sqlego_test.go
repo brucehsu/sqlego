69b1e2ca8664dbf87cf59994fb6a1b28d80c5f26
Szu-Kai Hsu (brucehsu) <brucehsu@gmail.com>
2015-01-15 11:23:48 +0800

    Add basic UPDATE statement testing
    Since the order of map entries could change randomly, we have to test each argument separately.

--- sqlego_test.go
+++ sqlego_test.go
@@ -1,11 +1,22 @@
 package sqlego
 
-import "testing"
+import (
+	s "strings"
+	"testing"
+)
 
 func TestSelectStatement(t *testing.T) {
 	node := Select("Users", []string{"id", "name", "email"})
 	sql := node.Compile()
 	if sql != "SELECT id,name,email FROM Users;" {
+		t.Fatal(sql)
+	}
+}
+
+func TestUpdateStatement(t *testing.T) {
+	node := Update("Users", map[string]string{"id": "2", "name": "Bruce", "email": "bruce@example.com"})
+	sql := node.Compile()
+	if !s.Contains(sql, "UPDATE Users SET") && !s.Contains(sql, "id=2") && !s.Contains(sql, "name=Bruce") && !s.Contains(sql, "email=bruce@example.com") {
 		t.Fatal(sql)
 	}
 }
