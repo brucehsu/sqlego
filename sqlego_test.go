4377207f90f7818668fa86732e3e8e7db6c867a5
Szu-Kai Hsu (brucehsu) <brucehsu@gmail.com>
2015-01-15 11:43:40 +0800

    Add basic SELECT statement with WHERE testing

--- sqlego_test.go
+++ sqlego_test.go
@@ -13,6 +13,15 @@ func TestSelectStatement(t *testing.T) {
 	}
 }
 
+func TestSelectStatementWithWhere(t *testing.T) {
+	node := Select("Users", []string{"id", "name", "email"})
+	node.Where(Gte("id", "10"))
+	sql := node.Compile()
+	if sql != "SELECT id,name,email FROM Users WHERE id>=10;" {
+		t.Fatalf("Single condition:\n%s", sql)
+	}
+}
+
 func TestInsertStatement(t *testing.T) {
 	node := Insert("Users", map[string]string{"name": "Bruce"})
 	sql := node.Compile()
